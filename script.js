7034b3f8737e893bf041b125d397509a36e1fca4
Arthutepio <arthurodriguesr10@gmail.com>
2022-08-11 14:50:58 -0300

    requisito 5

--- script.js
+++ script.js
@@ -28,6 +28,10 @@ const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').inn
 
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
+// console.log(event);
+const clean = event.target;
+// console.log(clean);
+clean.remove();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
