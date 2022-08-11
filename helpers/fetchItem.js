830208e34fcf293095c54c4357c1a3c1f1bafa65
Arthutepio <arthurodriguesr10@gmail.com>
2022-08-11 13:12:53 -0300

    requisito 4

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -4,7 +4,7 @@ const fetchItem = async (item) => {
     const url = `https://api.mercadolibre.com/items/${item}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log('item', data);
+    // console.log('item', data);
     return data;
   } catch (error) {
     return error;
--- script.js
+++ script.js
@@ -38,21 +38,51 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-const getProduct = async (item) => {
-  const itensSection = document.querySelector('.items');
-  const result = await fetchProducts(item);
+// ==============requisito 3=====================//
+
+const getProduct = async () => {
+  const itens = document.querySelector('.items');
+  const result = await fetchProducts('computador');
   result.results.forEach((e) => {
     const createProduct = createProductItemElement({
       sku: e.id, 
       name: e.title, 
       image: e.thumbnail,
     });
-    itensSection.appendChild(createProduct);
+    itens.appendChild(createProduct);
   });
 };
 
-window.onload = async () => {
-   await getProduct('computador');
+// ==============requisito 4=====================//
+
+const btnAddCartShopping = document.querySelector('.items');
+const cartShoppingArea = document.querySelector('.cart__items');
+
+const addToCart = async (id) => {
+  const result = await fetchItem(id);
+  // console.log(result);
+  const createObjectProduct = { 
+    sku: result.id, 
+    name: result.title, 
+    salePrice: result.price,
+  };
+  // console.log(createObjectProduct); // retorna o obj
+  cartShoppingArea.appendChild(createCartItemElement(createObjectProduct));
+};
+
+btnAddCartShopping.addEventListener('click', (event) => {
+  const btnClick = event.target;
+  // console.log(btnClick.className); // item__add
+  if (btnClick.className === 'item__add') {
+    // console.log(event.target.parentNode);
+    const id = getSkuFromProductItem(event.target.parentNode);
+    // console.log(id); // retorna o id
+    addToCart(id);
+  }
+});
+
+window.onload = () => {
+   getProduct();
   //  fetchProducts('computador');
   // await fetchItem('MLB1615760527');
 };
